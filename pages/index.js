10e96c84ac4ec7b1a07fa32e755ee4ecc225bf7d
Rafael Tavares <83890454+Rafael02tavares@users.noreply.github.com>
2021-07-23 22:00:10 -0300

    construção de pagina de login
    foi criado alguns textos na pagina de login

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,42 @@ import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const login = (props) => {
   return (
-    <Form inline>
-      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Label for="exampleEmail" className="mr-sm-2">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
-      </FormGroup>
-      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Label for="examplePassword" className="mr-sm-2">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
-      </FormGroup>
-      <Button>Submit</Button>
-    </Form>
+    <div>
+      <div class="container">
+        <Form id="">
+          <FormGroup>
+            <h3> Login </h3>
+            <p>
+              Voce não tem uma conta ainda? Create sua conta aqui em poucos minutos
+            </p>
+            <Label for="exampleEmail" >Email</Label>
+            <Input type="email" name="email" id="Email" placeholder="email@gmail.com" />
+          </FormGroup>
+          <br></br>
+          <FormGroup>
+            <Label for="Password">Password</Label>
+            <Input type="password" name="password" id="examplePassword" placeholder="sua senha aqui" />
+          </FormGroup>
+          <label>Lembre minha senha
+            <input type="checkbox"></input>
+          </label>
+          <p>esqueci minha senha</p>
+          <Button>Submit</Button>
+        </Form>
+      </div>
+      <div>
+          <h2>Bem vindo ao Guru Sports</h2>
+          <p>tenha os melhores palpites de placar sporte é informativos sobre as rodadas.</p>
+          <p>faça login com</p>
+          <a href="#">
+          <i class="fa fa-facebook fa-fw"></i> Login com Facebook
+         </a>
+         <br></br>
+         <a href="#" >
+            <i></i> Login com conta google
+         </a>
+      </div>
+    </div>
   );
 }
 
